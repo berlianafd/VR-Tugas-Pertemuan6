4d1f50544bae87ee43dc0aa5843b368240ef6719
berlianafd <berlianafd@gmail.com>
2020-10-05 11:01:59 +0700

    Initial Commit

---
+++ Assets/Scripts/VRGaze.cs
@@ -0,0 +1,60 @@
+﻿﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VRGaze : MonoBehaviour
+{
+    public Image imgGaze;
+    public Text CubeMerah;
+    public Text CubeHitam;
+    public float totalTime = 2;
+    bool gvrStatus, gvrStatus1;
+    float gvrTimer;
+ 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+       CubeMerah.enabled = false;
+       CubeHitam.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gvrStatus)
+        {
+            gvrTimer += Time.deltaTime;
+            imgGaze.fillAmount = gvrTimer / totalTime;
+        }
+    }
+
+    public void GVRON()
+    {
+        gvrStatus = true;
+    }
+
+    public void GVROff()
+    {
+        gvrStatus = false;
+        gvrTimer = 0;
+        imgGaze.fillAmount = 0;
+    }
+
+    public void CubeMerahOn(){
+    	CubeMerah.enabled = true;
+    }
+
+    public void CubeMerahOff(){
+    	CubeMerah.enabled = false;
+    }
+
+    public void CubeHitamOn(){
+    	CubeHitam.enabled = true;
+    }
+
+    public void CubeHitamOff(){
+    	CubeHitam.enabled = false;
+    }
+}
---
+++ Assets/Scripts/menampilkanText.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class menampilkanText : MonoBehaviour
+{
+
+	public Text pertemuan3;
+    public Text pertemuan4;
+    public Text pertemuan5;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+     	pertemuan3.enabled = false;
+     	pertemuan4.enabled = false;
+     	pertemuan5.enabled = false;   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void pertemuan3On(){
+    	pertemuan3.enabled = true;
+    }
+
+    public void pertemuan3Off(){
+    	pertemuan3.enabled = false;
+    }
+
+    public void pertemuan4On(){
+    	pertemuan4.enabled = true;
+    }
+
+    public void pertemuan4Off(){
+    	pertemuan4.enabled = false;
+    }
+
+    public void pertemuan5On(){
+    	pertemuan5.enabled = true;
+    }
+
+    public void pertemuan5Off(){
+    	pertemuan5.enabled = false;
+    }
+}
